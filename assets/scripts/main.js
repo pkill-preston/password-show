c53f8e00a16031b8f685e335aac9d9209bdb09ab
pkill-preston <0v3rthinker@protonmail.com>
2021-03-19 08:02:09 -0300

    Definitive script

---
+++ assets/scripts/main.js
@@ -0,0 +1,21 @@
+var labelChecker = document.getElementsByClassName("labelChecker")[0];
+var imageChecker = document.getElementsByClassName("imageChecker")[0];
+
+
+
+function checking()
+{
+    let password = document.getElementById("password");
+
+    if(password.type === 'password')
+    {
+        password.type = 'text';
+        imageChecker.style.display = "none";
+    }else
+    {
+        password.type = 'password';
+        labelChecker.style.display = "flex";
+        imageChecker.style.display = "flex";
+    }
+
+}
